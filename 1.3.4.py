3945807d045d7010ff54c6e8a738d71c6fda0d2f
Student <93788062+Oskarmaker@users.noreply.github.com>
2024-04-04 15:35:05 +0300

    Update 1.3.4.py

--- 1.3.4.py
+++ 1.3.4.py
@@ -3,7 +3,7 @@ matrix=zeros((10,10))
 l1,l2=10,10
 for line in range(l1):
     for row in range(l2):
-        if line%(l1-1)*row%(l2-1)==0:
+        if ((line % (l1 - 1)) * (row % (l2 - 1)))==0:
             matrix[line][row]=1
         # print(line%(l1-1),row%(l2-1))
 print(matrix)
