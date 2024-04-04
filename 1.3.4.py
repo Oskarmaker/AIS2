52abb445c52c29e64ec60b3c659a3c8828e5a0dd
Student <93788062+Oskarmaker@users.noreply.github.com>
2024-04-04 15:35:48 +0300

    1.3.4.py

--- 1.3.4.py
+++ 1.3.4.py
@@ -5,5 +5,4 @@ for line in range(l1):
     for row in range(l2):
         if ((line % (l1 - 1)) * (row % (l2 - 1)))==0:
             matrix[line][row]=1
-        # print(line%(l1-1),row%(l2-1))
 print(matrix)
